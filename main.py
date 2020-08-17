d32e0c6717a1c74bfacaffa18da685b2c76804b3
Atkrista <atkristak@gmail.com>
2020-08-17 15:29:14 +0545

    finished board renderer

---
+++ main.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+HEIGHT = 10
+WIDTH = 10
+
+class Game:
+    def __init__(self, height = HEIGHT, width = WIDTH):
+        self.height = height
+        self.width = width
+
+    def board_matrix(self):
+        matrix = np.zeros(shape = (self.height, self.width), dtype = int)
+        return matrix
+
+
+    def render(self, board):
+        #print the line on the top of the board
+        for i in range(len(board)):
+            print("__ ", end ='')
+        print()    
+        #Print the actual board 
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 0:
+                    print('.  ', end = '')
+                else:
+                    print('#', end = '')
+            print()
+
+        #Print the line at the bottom of the board
+        for i in range(len(board)):
+            print("__ ", end ='')
+        print()
+
+    
+
+
+game = Game(10,10)
+board1 = game.board_matrix()
+game.render(board1)
+
